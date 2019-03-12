6c26ce58ee61ad82391d852897ca6be08c57a5ea
bestvist <bestvist@qq.com>
2019-03-13 07:38:33 +0800

    add rangeRowValue

--- src/ctrl.js
+++ src/ctrl.js
@@ -75,33 +75,28 @@ class ctrl {
         return Math.random() < 0.9 ? 2 : 4;
     }
 
-    rangeRowValue(arr) {
-        //遍历数组从数组的的当前位置的下一个开始遍历，找不是0的位置()
+    rangeRowValue(arr=[]) {
+        // 遍历数组从数组的的当前位置的下一个开始遍历，找不是0的位置()
         // 如果没找到什么也不做
         // 如果找到
-        //如果当前位置是0，那么像当前位置与下一个进行互换（当前位置获得下一个位置的数据，并且将下一个位置数据置为0，将下标减一）
-        //如果当前位置和下一个位置相等，将当前位置数据*2，下个位置数据置0
-        var i, nextI, len, m;
-        len = arr.length;
-        for (i = 0; i < len; i += 1) {
-            //先找nextI
-            nextI = -1;
-            for (m = i + 1; m < len; m++) {
-                if (arr[m] !== 0) {
-                    nextI = m;
-                    break;
-                }
-            }
+        // 如果当前位置是0，那么像当前位置与下一个进行互换（当前位置获得下一个位置的数据，并且将下一个位置数据置为0，将下标减一）
+        // 如果当前位置和下一个位置相等，将当前位置数据*2，下个位置数据置0
+        let next;
+        for (let i = 0; i < arr.length; i++) {
+            // 先找next
+            next = arr.findIndex((c,m)=>{
+				return m >i && c!==0;
+			})
 
-            if (nextI !== -1) {
-                //存在下个不为0的位置
+            if (next !== -1) {
+                // 存在下个不为0的位置
                 if (arr[i] === 0) {
-                    arr[i] = arr[nextI];
-                    arr[nextI] = 0;
+                    arr[i] = arr[next];
+                    arr[next] = 0;
                     i -= 1;
-                } else if (arr[i] === arr[nextI]) {
+                } else if (arr[i] === arr[next]) {
                     arr[i] = arr[i] * 2;
-                    arr[nextI] = 0;
+                    arr[next] = 0;
                 }
             }
         }
